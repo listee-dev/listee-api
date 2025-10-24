6603bf4da4b0d5b76bddb16666f76194773853ba
Gen Tamura <gen.tamura84@gmail.com>
2025-10-24 15:26:59 +0900

    feat: support update and delete operations

--- src/app/api/categories/[categoryId]/route.ts
+++ src/app/api/categories/[categoryId]/route.ts
@@ -3,3 +3,11 @@ import { dispatchToListeeApi } from "@/app/api/handler";
 export async function GET(request: Request): Promise<Response> {
   return await dispatchToListeeApi(request);
 }
+
+export async function PATCH(request: Request): Promise<Response> {
+  return await dispatchToListeeApi(request);
+}
+
+export async function DELETE(request: Request): Promise<Response> {
+  return await dispatchToListeeApi(request);
+}
